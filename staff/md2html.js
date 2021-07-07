5796ef303e28567a7da9595d24aab25d6a771d4a
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-07-08 08:07:15 +0900

    add materialize-css

--- staff/md2html.js
+++ staff/md2html.js
@@ -9,8 +9,12 @@ const inputContent = fs.readFileSync(inFile, 'utf8')
 const renderer = new marked.Renderer();
 const heading = renderer.heading.bind(renderer)
 renderer.heading = (text, level, raw, slugger) => {
-    console.log("level="+level)
-    let cls = (level == 1) ? ` class="matter-h${level} matter-primary">` : ` class="matter-h${level}">`
+    let cls = ` class="matter-h${level}">`
+    if (level == 1) {
+        cls = ` class="matter-h${level} matter-primary-text">`
+    } else if (level == 2) {
+        cls = ` class="matter-h${level} matter-secondary-text">`
+    }
     return heading(text, level, raw, slugger).replace(/>/i, cls)
 }
 const link = renderer.link.bind(renderer);
@@ -25,10 +29,16 @@ const content = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <title>Learn Languages 2021</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 <link href="https://res.cloudinary.com/finnhvman/raw/upload/matter/matter-0.2.2.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
-<body class="matter-body1">
+<body>
+<span class="material-body1">
 ${marked(inputContent)}
+</span>
+<script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>`
 
