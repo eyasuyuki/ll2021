540a1789f27aa384027a74b707189ba949d625a4
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-08-01 08:21:35 +0900

    add Dart mindmap memo, version history (still working).

---
+++ speakers/eyasuyuki/Dart.mm
@@ -0,0 +1,93 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Dart" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1627636620421" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="言語仕様" POSITION="right" ID="ID_1951428256" CREATED="1627636889757" MODIFIED="1627657066208">
+<edge COLOR="#00ff00"/>
+<node TEXT="non-null by default" ID="ID_1537464756" CREATED="1627636623972" MODIFIED="1627657069167">
+<node TEXT="文法の概要は?" ID="ID_1974283853" CREATED="1627636799889" MODIFIED="1627636824864"/>
+<node TEXT="既存のコードへの影響は?" ID="ID_971271383" CREATED="1627636827204" MODIFIED="1627636840286"/>
+<node TEXT="ライブラリの対応状況は?" ID="ID_1697297713" CREATED="1627636842116" MODIFIED="1627636849092"/>
+</node>
+<node TEXT="const式" ID="ID_986997372" CREATED="1627639217519" MODIFIED="1627639224614"/>
+<node TEXT="required" ID="ID_1578734543" CREATED="1627636896206" MODIFIED="1627636900586"/>
+</node>
+<node TEXT="コンパイラ" POSITION="left" ID="ID_1794288813" CREATED="1627656918418" MODIFIED="1627657024576">
+<edge COLOR="#00ffff"/>
+<node TEXT="dart2native" ID="ID_1364154762" CREATED="1627657026659" MODIFIED="1627657034116"/>
+</node>
+<node TEXT="サーバーサイド" POSITION="left" ID="ID_1745847563" CREATED="1627636652216" MODIFIED="1627636660918">
+<edge COLOR="#0000ff"/>
+<node TEXT="サーバーサイドもDartの人/企業は実在する?" ID="ID_175612592" CREATED="1627636715711" MODIFIED="1627636787350"/>
+<node TEXT="Node.js?" ID="ID_1583018465" CREATED="1627636754605" MODIFIED="1627636765300"/>
+<node TEXT="AWS Lambda?" ID="ID_1028381646" CREATED="1627636768090" MODIFIED="1627636772530"/>
+</node>
+</node>
+</map>
---
+++ speakers/eyasuyuki/README.md
@@ -0,0 +1,15 @@
+Dart Language Update
+====
+
+# 前回のLanguage Updateからのバージョンの変遷
+
+https://github.com/dart-lang/sdk/blob/master/CHANGELOG.md
+
+|リリース日|バージョン|備考|
+|:---|:---|:---|
+|2019-08-07|2.4.1|AOTコンパイラのパフォーマンス向上、JSラッパーの厳密化、バグ修正等|
+|(2019-08-24)| |([Learn Languages 2019](https://ll.jus.or.jp/2019/) 開催)|
+|2019-09-10|2.5.0|```const```式による明示的なキャスト、```List<int>```を返していたコアライブラリが```Uint8List```を返すようになった(破壊的変更)、```Uri```クラスのRFC6874サポート、```Cookie```コンストラクタ引数の破壊的変更、[pub.dev](https://pub.dev/) パッケージのGit依存排除、Linter```0.1.96```でいくつかの改良、Dartdoc ```0.28.4```でのいくつかの改良|
+|2019-09-27|2.5.1|バグ修正等|
+|2019-10-08|2.5.2|バグ修正等|
+|2019-11-05|2.6.0|静的extentionメンバー、```FutureOr```コンテキストでの```Null```値の使用の破壊的変更、ネイティブコンパイラ```dart2native```の追加、```dartfmt```コマンドがdeprecatedに(代替は```dart format```)、```dart create```コマンドで```package:lints```を使うように、|
